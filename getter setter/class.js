eb51dcd3ca3b6cb79ed8ab9e3b663e53ae8cb67a
roushan-sheik <roushansheik@gmail.com>
2024-02-03 08:41:01 +0600

    Getter setter with class

---
+++ getter setter/class.js
@@ -0,0 +1,26 @@
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    console.log(this);
+  }
+  //NOTE - Setter
+  set width(newWidth) {
+    this._width = newWidth;
+  }
+  set height(newHeight) {
+    this._height = newHeight;
+  }
+  //NOTE -  Getter
+  get width() {
+    return this._width;
+  }
+  get height() {
+    return this._height;
+  }
+}
+
+const rect = new Rectangle(0, 0);
+
+console.log(rect.width);
+console.log(rect.height);
