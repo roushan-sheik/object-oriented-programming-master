efa5a80a590c6235d0379e7d17cd4e7a04887d2b
roushan-sheik <roushansheik@gmail.com>
2024-02-03 07:09:05 +0600

    Abstraction method implemented added

--- abstraction/abstraction.js
+++ abstraction/abstraction.js
@@ -1,14 +1,21 @@
 function Rectangel(width, height) {
   this.width = width;
   this.height = height;
+  let position = {
+    x: 50,
+    y: 80,
+  };
   this.draw = function () {
     console.log("I am drawing rectanlge....");
-    console.log(this.printProperties);
+    console.log(printProperties());
   };
-  this.printProperties = function () {
+  const printProperties = function () {
+    console.log(this);
     console.log("Height = ", this.height);
     console.log("Width = ", this.width);
-  };
+      console.log( "Position X = ", position.x, "position Y = ", position.y );
+      return "Printing...."
+  }.bind(this);
 }
 const rect = new Rectangel(50, 100);
-rect.printProperties();
+rect.draw();
