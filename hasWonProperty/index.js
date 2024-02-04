440bb055b391117d71d277d3ed0057bea0448d88
roushan-sheik <roushansheik@gmail.com>
2024-02-04 16:31:12 +0600

    To see instance methods and all prototypical method

--- hasWonProperty/index.js
+++ hasWonProperty/index.js
@@ -20,5 +20,14 @@ let obj = {
 //* তার মানে যদি existing property টা  existing অবজেক্ট এ থাকে তাহলে,
 //* hasOwnProperty(””) টা true রিটার্ন করবে।
 // let result = obj.hasOwnProperty("name");// true
-let result = rect1.hasOwnProperty("getWidth");// true
+let result = rect1.hasOwnProperty("getWidth"); // true
 console.log(result);
+//NOTE -
+//* শুধুমাত্র instance/ নিজস্ব methods গুলো দেখার জন্য Object.keys() ব্যবহার করতে পারি।
+let instMethods = Object.keys(rect1);
+console.log(instMethods); // ['width', 'getWidth']
+//NOTE -
+//* instance এবং prototypical methods সব গুলো দেখার জন্য ফর ইন লুপ ব্যবহার করতে পারি।
+for (const methods in rect1) {
+  console.log(methods); 
+}
