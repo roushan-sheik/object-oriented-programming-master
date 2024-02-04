5c3cdc7f92fa792052ac7555a00d8d0a98759acb
roushan-sheik <roushansheik@gmail.com>
2024-02-04 06:42:53 +0600

    Inheritance added

---
+++ inheritance/index.js
@@ -0,0 +1,5 @@
+//NOTE - In programming, inheritance refers to passing down characteristics from a parent to a child so that a new piece of code can reuse and build upon the features of an existing one.
+//NOTE - We have 2 kinds of inheritance in javascript
+//* 1. Prototypical inheritance 
+//* 2. Classical inheritance 
+ 
