7bec421c1e2299ebae55dc2a94bafc223694ac29
roushan-sheik <roushansheik@gmail.com>
2024-02-04 15:26:09 +0600

    Protypical inheritance added

--- instance_vs_prototype/index.js
+++ instance_vs_prototype/index.js
@@ -1,10 +1,15 @@
 // Instance  Vs Prototype
-function Rectangle ( width )
-{
+function Rectangle(width) {
   this.width = width;
-  this.draw = function () {
-    console.log("drawing...");
-  };
 }
+//* Rectangle এর prototype এর ভেতরে কমন মেথড draw টা কে সেট করা হয়েছে।
+//* এখন Rectangle কনস্ট্রাক্টর দিয়ে যত অবজেক্ট তৈরী হবে draw টা কমন মেথড হবে।
+Rectangle.prototype = {
+  draw: function () {
+    console.log("drawing...");
+  },
+};
 const rect1 = new Rectangle(100);
 rect1.draw();
+const rect2 = new Rectangle(50);
+rect2.draw();
