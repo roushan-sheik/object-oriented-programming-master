b2915bdfce6297b9815df810fd54dbcc4034af0d
roushan-sheik <roushansheik@gmail.com>
2024-02-04 07:56:47 +0600

    Multilevel inheritance added

--- multilevel_inheritance/index.js
+++ multilevel_inheritance/index.js
@@ -1,1 +1,5 @@
 //* Multilevel Inheritance
+let myArr = [];
+//* এই myArr প্রেটো টা হচ্ছে একটা প্যরেন্ট অবজেক্ট
+//* যার ভেতরে [Array] এর সকল মেথডস গুলো associate হয়ে আছে
+//* আবার এই  __proto__ টা Original {} কে Inherit করছে।
