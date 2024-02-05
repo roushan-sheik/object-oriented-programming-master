ae75a15b7dcedf6b45c52375e692c5ab8460836d
roushan-sheik <roushansheik@gmail.com>
2024-02-05 07:17:48 +0600

    come back to our pure prototypical inheritance

--- prototypical_ineheritance/reset-parent-prototype.js
+++ prototypical_ineheritance/reset-parent-prototype.js
@@ -1,10 +1,8 @@
 // Shape constructor which will contain all the common methods
 function Shape() {}
 // Set Shape Prototype
-Shape.prototype = {
-  commonMethod: function () {
-    console.log("I am a common Method");
-  },
+Shape.prototype.commonMethod = function () {
+  console.log("I am a common Method");
 };
 // Create a rectangle class to create rect.
 function Rectangle(width) {
@@ -16,13 +14,7 @@ Rectangle.prototype = Object.create(Shape.prototype);
 Rectangle.prototype.anotherMethod = function () {
   console.log("I am another method");
 };
-// reset parent prototype by using equal sign 
-Rectangle.prototype = {
-  heroMethod: function () {
-    console.log("I am Herro method.");
-  },
-};
-
+ 
 // create new object based on those 2 constructor
 const shape = new Shape();
 const rect = new Rectangle(100);
