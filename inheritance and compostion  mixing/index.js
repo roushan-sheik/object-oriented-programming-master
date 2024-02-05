a2280b5e58ad85c85bf5e805e9ff09ace6cfb4d3
roushan-sheik <roushansheik@gmail.com>
2024-02-05 23:53:06 +0600

    composition and mixin added

--- inheritance and compostion  mixing/index.js
+++ inheritance and compostion  mixing/index.js
@@ -1,3 +1,7 @@
+// Mixin
+function mixin(target, ...task) {
+  Object.assign(target, ...task);
+}
 let canEat = {
   eat: function () {
     console.log("Eating...");
@@ -8,13 +12,29 @@ let canWalk = {
     console.log("Walking...");
   },
 };
+// Gold fish
+let canSwim = {
+  swim: function () {
+    console.log("Swiming...");
+  },
+};
 // Create a Person Obj who can eat and walk
 
 function Person(name) {
   this.name = name;
 }
-Object.assign(Person.prototype, canEat, canWalk);
-
+// Object.assign(Person.prototype, canEat, canWalk);
+mixin(Person.prototype, canWalk, canEat);
+// Our new Gold fish features
+function GoldFish(name) {
+  this.name = name;
+}
+mixin(GoldFish.prototype, canEat, canSwim);
+// Create new person object
 const p1 = new Person("Tasmiya");
-p1.eat();
-p1.walk();
+// p1.eat();
+// p1.walk();
+// Create new gold fish object
+const fish = new GoldFish("Gold Fish");
+// fish.eat();
+// fish.swim();
