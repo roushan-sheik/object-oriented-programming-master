e80aec04e184db9bcaae1b00a365da45e9d295b3
roushan-sheik <roushansheik@gmail.com>
2024-02-06 07:28:00 +0600

    Power of new keyword

--- ___final_over_view___/person.js
+++ ___final_over_view___/person.js
@@ -1,9 +1,7 @@
 // Person constructor
 function Person(name) {
   //* Object.create মেথড টা BasePerson এর prototype টা কে inherit করবে।
-  let person = Object.create(Person.prototype);
-  person.name = name;
-  return person;
+  this.name = name;
 }
 // Set the prototype to Person and make life easer
 Person.prototype = {
@@ -15,10 +13,10 @@ Person.prototype = {
   },
 };
 
-const p1 = Person("Tasmiya");
+const p1 = new Person("Tasmiya");
 console.log(p1);
 p1.eat();
 // person 2
-const p2 = Person("Arifa Moni");
+const p2 = new Person("Arifa Moni");
 console.log(p2);
 p1.sleep();
